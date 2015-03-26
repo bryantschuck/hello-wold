bd485b6898a01512b2744f6a5b24a9e2346bf5eb
cah-bryantschuck <cah-bryantschuck@users.noreply.github.com>
2015-03-26 14:32:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9 this is for Jenkins\n');
+  res.end('Hello World from Cloud9 this is for Jenkins, work!\n');
 }).listen(process.env.PORT);
